04b3e03aed20d9093f9414ef69e688a39fa8fad9
Kriths2430 <Krithrav1990@gmail.com>
2026-03-11 13:35:22 +0530

    Trigger 9

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template("apple.html")
 
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
